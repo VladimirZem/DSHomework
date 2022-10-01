5499dbecdfe4c3094177aa7655d2d3385b925fe0
VladimirZem <112812217+VladimirZem@users.noreply.github.com>
2022-10-01 23:51:03 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('#worksSlider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+  });
+//   if ($(window).width()>1300){
+//     ...execute script
+// }
+})
+
+
